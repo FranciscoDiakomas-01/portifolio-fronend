f7c63aefe01bee28b68a7e0d3edc05e15b253f3a
Francisco <franciscodiakoma@gmail.com>
2024-12-20 06:30:26 +0100

    terminei

--- src/services/getProjects.js
+++ src/services/getProjects.js
@@ -1,7 +1,9 @@
 /* eslint-disable no-unused-vars */
 export default async function getProjects() {
     try {
-        const API = await fetch("http://localhost:8000/projects");
+        const API = await fetch(
+          "https://portfolio-backend1-xh2e.onrender.com/projects"
+        );
         const response = await API.json()
         return response.data
         
